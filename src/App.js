43524a9cefaebf5dd79740424941d0f6b3a55527
shrikajol <shrikajol@gmail.com>
2022-12-21 11:18:41 +0600

    swr ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import useSwr from "swr";
+import "./App.css";
+
+const fetchData = (url) => fetch(url).then((response) => response.json());
 
 function App() {
+  const { data, error, isLoading } = useSwr(
+    "https://dummyjson.com/products/1",
+    fetchData
+  );
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Home</div>
     </div>
   );
 }
